684e8854921670d2df95052a65aa3293110b1da6
Altamash Ansari <altamash.ansari@raweng.com>
2019-05-13 13:43:13 +0530

    initial comment

---
+++ precache-manifest.9afa369a450c1d7da9d9d96a2009c9ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ad4e6a41eb42562c5cd2801983bf40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2877e7f0377c9ba6084c",
+    "url": "/static/css/main.33fbf719.chunk.css"
+  },
+  {
+    "revision": "6216d881b93bbd3e9003",
+    "url": "/static/js/2.4426cc89.chunk.js"
+  },
+  {
+    "revision": "2877e7f0377c9ba6084c",
+    "url": "/static/js/main.ee7117c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
